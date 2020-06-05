b98ed29ff07bd6ece77daba807531582537353ed
keshuook <amritanshuanita@gmail.com>
2020-06-05 20:20:56 +0530

    index.html done

--- user/vidyutsid/projects/custom website/script.js
+++ user/vidyutsid/projects/custom website/script.js
@@ -8,13 +8,18 @@ function loaded(){
 window.onscroll = function() {Function()};
 
 var header = document.getElementById("top-nav");
+var side = document.getElementById("side-bar");
 var sticky = header.offsetTop;
 
 function Function() {
   if (window.pageYOffset > sticky) {
     header.classList.add("sticky");
+    header.classList.add("sticky-top");
+    side.classList.add("sticky");
   } else {
     header.classList.remove("sticky");
+    header.classList.remove("sticky-top");
+    side.classList.remove("sticky");
   }
 }
 setInterval(function(){
